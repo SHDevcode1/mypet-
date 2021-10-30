144209376b52e21cec9b7588fcb37dbffbac378f
Juan Gabriel <Perezjuang@hotmail.com>
2021-10-30 09:48:19 -0500

    Update AppRoutes.js

--- aprymypetfront/src/routes/AppRoutes.js
+++ aprymypetfront/src/routes/AppRoutes.js
@@ -1,21 +1,18 @@
 import React from 'react';
-
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Layout from "../containers/LayoutPrincipal/LayoutPrincipal";
 import Home from "../components/home/Home";
-import Employee from "../components/employee/Employee";
-import NoFound from "../components/notfound/NoFound";
+//import Employee from "../components/employee/Employee";
+//import NoFound from "../components/notfound/NoFound";
 
-
-const AppRoutes = () => {
-    <BrowserRouter>
+export default function AppRoutes() {
+    return (
+    <Router>
         <Layout>
             <Switch>
-                <Route exact path="/" component = {Home} />
-                <Route exact path="/Employee" component = {Employee} />
-                <Route component = {NoFound} />
+                <Route path="/" component = {Home} />
             </Switch>
         </Layout>
-    </BrowserRouter>
+    </Router>
+    );
 }
-export default AppRoutes;
