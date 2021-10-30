b8fa09332683bbc71b7f1197cad236e8b7c428f1
sah98 <sahz1998@gmail.com>
2021-10-30 08:26:01 -0500

    Commit 2

--- aprymypetfront/src/components/employee/EmployeeTable.jsx
+++ aprymypetfront/src/components/employee/EmployeeTable.jsx
@@ -1,28 +1,49 @@
 import React from "react";
 
-const EmployeeTable = () => (
+const EmployeeTable = (props) => (
   <table>
     <thead>
       <tr>
         <th>ID BD</th>
-        <th>first_name</th>
-        <th>last_name</th>
+        <th>firstName</th>
+        <th>lastName</th>
         <th>email</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>ID BD</td>
-        <td>first_name</td>
-        <td>last_name</td>
-        <td>email</td>
-        <td>
-          <button className="button muted-button">Edit</button>
-          <button className="button muted-button">Delete</button>
-        </td>
-      </tr>
+      {undefined !== props.employees && props.employees.length > 0 ? (
+        props.employees.map((employee) => (
+          <tr key={employee.id.toString()}>
+            <td>{employee.id}</td>
+            <td>{employee.firstName}</td>
+            <td>{employee.lastName}</td>
+            <td>{employee.email}</td>
+            <td>
+              <button
+                onClick={() => {
+                  props.editRow(employee);
+                }}
+                className="button muted-button"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => props.deleteEmployee(employee.id)}
+                className="button muted-button"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No employee</td>
+        </tr>
+      )}
     </tbody>
   </table>
 );
 
 export default EmployeeTable;
+
