5a17e51dad4e081ffcea93091357e2d6206395dd
sah98 <sahz1998@gmail.com>
2021-11-13 10:32:01 -0500

    Commit4

--- demoSHD/src/main/java/com/SHD/GetMapping.java
+++
@@ -1,5 +0,0 @@
-package com.SHD;
-
-public @interface GetMapping {
-
-}
--- ptymypetfront/src/containers/app/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const App = () => (
-    <div className="App">
-        HOLA MUNDO
-    </div>
-)
-export default App;
