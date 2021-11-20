79a682fe3c6bb67600ef579e4f2840d8f16561a9
SHDevcode1 <shoyosd0@gmail.com>
2021-11-20 09:41:24 -0500

    de todo
    no había hecho commit

---
+++ prymypetfront/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './Home.scss';
+
+const Home = () => (
+    <div className="SinEspecificar">
+        Buenasss, se dice los del home
+    </div>
+)
+export default Home;
