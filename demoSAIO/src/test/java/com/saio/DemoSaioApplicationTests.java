52e892bc496f8fe9c573526fb3c658de8aec7624
santiagoisaza0 <azasantis@hotmail.com>
2021-11-20 09:56:20 -0500

    20/11/2021
    sa

---
+++ demoSAIO/src/test/java/com/saio/DemoSaioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSaioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
